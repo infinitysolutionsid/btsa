38d0d6fef78cf07727e6eb8cd49500e283558c72
Star CodeX <hello@bintangtobing.com>
2019-12-09 18:11:51 +0700

    update

--- resources/views/dash/index.blade.php
+++ resources/views/dash/index.blade.php
@@ -137,6 +137,21 @@
                             <div class="testimonial-author-position">Founder-Ceo. Dell Corp</div>
                         </div>
                     </div>
+                    <div class="item">
+                        <div class="testimonial-content">
+                            <div class="testimonial-text">
+                                <i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur
+                                adipisicing
+                                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad
+                                minim veniam, quis
+                                nostrud exercitation <i class="fa fa-quote-right"></i>
+                            </div>
+                            <img class="testimonial-author-img" src="{{$IRModel->getAvatar()}}" alt="" />
+                            <div class="testimonial-author">TYRION LANNISTER</div>
+                            <div class="testimonial-author-position">Founder-Ceo. Dell Corp</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
