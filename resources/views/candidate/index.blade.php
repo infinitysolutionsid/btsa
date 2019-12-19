b553a1a02022b9ce945064f41ad5fbaf54ca0c6a
Star CodeX <hello@bintangtobing.com>
2019-12-19 09:35:04 +0700

    update

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -41,7 +41,7 @@
                         <div class="form-row">
                             <label for="" class="my-1 mr-2">Posisi yang dilamar</label>
                             <select name="appliedposition" id="" class="custom-select my-1 mr-sm-2">
-                                <option selected>Pilih salah satu...</option>
+                                <option selected disabled>Pilih salah satu...</option>
                                 @if(count($loker)>0)
                                 @foreach ($loker as $lowongan)
                                 <option value="{{$lowongan->available_position}}">{{$lowongan->available_position}}
