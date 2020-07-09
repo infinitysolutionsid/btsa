68ee6b5c4a637d85643c7075d91f1253c1aa80eb
Star CodeX <hello@bintangtobing.com>
2020-07-09 09:43:50 +0700

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -102,7 +102,8 @@
                     <li><a href="/candidate" target="_blank">Karir</a></li>
                     @if(auth()->check())
                     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true"><span><img class="media-object" src="{{asset('file/'.auth()->user()->profilephoto)}}"></span>
+                            aria-haspopup="true"><span><img class="media-object"
+                                    src="{{asset('file/'.auth()->user()->profilephoto)}}"></span>
                             {{auth()->user()->nama_lengkap}}</a>
                         <ul class="dropdown-menu">
                             <li><a href="/dashboard"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
@@ -139,8 +140,8 @@
                                             </button></a>
                                     </div>
                                     <div class="col-sm-6">
-                                        <a href="/restricted"><button type="button"
-                                                class="btn btn-custom" style="width:270px; height:60px;">BTSA Restricted
+                                        <a href="/restricted"><button type="button" class="btn btn-custom"
+                                                style="width:270px; height:60px;">BTSA Restricted
                                                 Area<br>
                                                 <font class="personalonly"><i>-For Company Only-</i></font>
                                             </button></a>
@@ -1501,7 +1502,8 @@
             </div><br>
             <div class="row slideanim">
                 <div class="col-sm-4">
-                    <span style="color: #282662; font-size: 4rem;" alt="budaya perusahaan BTSA Logistics"><i class="fab fa-sketch"></i></span>
+                    <span style="color: #282662; font-size: 4rem;" alt="budaya perusahaan BTSA Logistics"><i
+                            class="fab fa-sketch"></i></span>
                     <h4 style="color: #212121;"><strong>Integrity</strong></h4>
                     <p>Memiliki sikap kejujuran, terbuka, bertanggung jawab dan dapat dipercaya dalam melayani pelanggan
                         internal dan eksternal.</p>
@@ -1835,8 +1837,9 @@
                                 title="Learn about us on Wiki">
                                 <i class="fab fa-wikipedia-w fa-2x" title="Add us on Facebook"
                                     style="color: #fff"></i></a></li>
-                        <li><a href="http://www.youtube.com/c/BTSALogisticsYourReliableLogisticsPartner" data-toggle="tooltip"
-                                target="_blank" class="myTooltip" data-placement="bottom" title="Watch us on Youtube">
+                        <li><a href="http://www.youtube.com/c/BTSALogisticsYourReliableLogisticsPartner"
+                                data-toggle="tooltip" target="_blank" class="myTooltip" data-placement="bottom"
+                                title="Watch us on Youtube">
                                 <i class="fab fa-youtube fa-2x" title="Add us on Facebook" style="color: #fff"></i></a>
                         </li>
                     </ul>
@@ -1884,173 +1887,19 @@
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126932.82811066089!2d106.81830748289255!3d-6.177467720022987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5761b0a7d4f%3A0xc028c76f0a420b47!2sPT.+BERLIAN+TRANSTAR+ABADI+JAKARTA+%2F+PPJK+JAKARTA!5e0!3m2!1sen!2sid!4v1552883361046"
                             width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></p>
                     <h4>TRANSLATE</h4>
-                    <!-- GTranslate: https://gtranslate.io/ -->
-                    <a href="#" onclick="doGTranslate('id|en');return false;" title="English" class="gflag nturl"
-                        style="background-position:-0px -0px;"><img src="//gtranslate.net/flags/blank.png" height="24"
-                            width="24" alt="English" /></a><a href="#" onclick="doGTranslate('id|fr');return false;"
-                        title="French" class="gflag nturl" style="background-position:-200px -100px;"><img
-                            src="//gtranslate.net/flags/blank.png" height="24" width="24" alt="French" /></a><a href="#"
-                        onclick="doGTranslate('id|de');return false;" title="German" class="gflag nturl"
-                        style="background-position:-300px -100px;"><img src="//gtranslate.net/flags/blank.png"
-                            height="24" width="24" alt="German" /></a><a href="#"
-                        onclick="doGTranslate('id|it');return false;" title="Italian" class="gflag nturl"
-                        style="background-position:-600px -100px;"><img src="//gtranslate.net/flags/blank.png"
-                            height="24" width="24" alt="Italian" /></a><a href="#"
-                        onclick="doGTranslate('id|pt');return false;" title="Portuguese" class="gflag nturl"
-                        style="background-position:-300px -200px;"><img src="//gtranslate.net/flags/blank.png"
-                            height="24" width="24" alt="Portuguese" /></a><a href="#"
-                        onclick="doGTranslate('id|ru');return false;" title="Russian" class="gflag nturl"
-                        style="background-position:-500px -200px;"><img src="//gtranslate.net/flags/blank.png"
-                            height="24" width="24" alt="Russian" /></a><a href="#"
-                        onclick="doGTranslate('id|es');return false;" title="Spanish" class="gflag nturl"
-                        style="background-position:-600px -200px;"><img src="//gtranslate.net/flags/blank.png"
-                            height="24" width="24" alt="Spanish" /></a>
+                    <div id="google_translate_element"></div>
 
-                    <style type="text/css">
-                        <!--
-                        a.gflag {
-                            vertical-align: middle;
-                            font-size: 24px;
-                            padding: 1px 0;
-                            background-repeat: no-repeat;
-                            background-image: url(//gtranslate.net/flags/24.png);
-                        }
-
-                        a.gflag img {
-                            border: 0;
-                        }
-
-                        a.gflag:hover {
-                            background-image: url(//gtranslate.net/flags/24a.png);
-                        }
-
-                        #goog-gt-tt {
-                            display: none !important;
-                        }
-
-                        .goog-te-banner-frame {
-                            display: none !important;
-                        }
-
-                        .goog-te-menu-value:hover {
-                            text-decoration: none !important;
-                        }
-
-                        body {
-                            top: 0 !important;
-                        }
-
-                        #google_translate_element2 {
-                            display: none !important;
-                        }
-
-                        -->
-                    </style>
-
-                    <br /><select onchange="doGTranslate(this);">
-                        <option value="">Select Language</option>
-                        <option value="id|af">Afrikaans</option>
-                        <option value="id|sq">Albanian</option>
-                        <option value="id|ar">Arabic</option>
-                        <option value="id|hy">Armenian</option>
-                        <option value="id|az">Azerbaijani</option>
-                        <option value="id|eu">Basque</option>
-                        <option value="id|be">Belarusian</option>
-                        <option value="id|bg">Bulgarian</option>
-                        <option value="id|ca">Catalan</option>
-                        <option value="id|zh-CN">Chinese (Simplified)</option>
-                        <option value="id|zh-TW">Chinese (Traditional)</option>
-                        <option value="id|hr">Croatian</option>
-                        <option value="id|cs">Czech</option>
-                        <option value="id|da">Danish</option>
-                        <option value="id|nl">Dutch</option>
-                        <option value="id|en">English</option>
-                        <option value="id|et">Estonian</option>
-                        <option value="id|tl">Filipino</option>
-                        <option value="id|fi">Finnish</option>
-                        <option value="id|fr">French</option>
-                        <option value="id|gl">Galician</option>
-                        <option value="id|ka">Georgian</option>
-                        <option value="id|de">German</option>
-                        <option value="id|el">Greek</option>
-                        <option value="id|ht">Haitian Creole</option>
-                        <option value="id|iw">Hebrew</option>
-                        <option value="id|hi">Hindi</option>
-                        <option value="id|hu">Hungarian</option>
-                        <option value="id|is">Icelandic</option>
-                        <option value="id|id">Indonesian</option>
-                        <option value="id|ga">Irish</option>
-                        <option value="id|it">Italian</option>
-                        <option value="id|ja">Japanese</option>
-                        <option value="id|ko">Korean</option>
-                        <option value="id|lv">Latvian</option>
-                        <option value="id|lt">Lithuanian</option>
-                        <option value="id|mk">Macedonian</option>
-                        <option value="id|ms">Malay</option>
-                        <option value="id|mt">Maltese</option>
-                        <option value="id|no">Norwegian</option>
-                        <option value="id|fa">Persian</option>
-                        <option value="id|pl">Polish</option>
-                        <option value="id|pt">Portuguese</option>
-                        <option value="id|ro">Romanian</option>
-                        <option value="id|ru">Russian</option>
-                        <option value="id|sr">Serbian</option>
-                        <option value="id|sk">Slovak</option>
-                        <option value="id|sl">Slovenian</option>
-                        <option value="id|es">Spanish</option>
-                        <option value="id|sw">Swahili</option>
-                        <option value="id|sv">Swedish</option>
-                        <option value="id|th">Thai</option>
-                        <option value="id|tr">Turkish</option>
-                        <option value="id|uk">Ukrainian</option>
-                        <option value="id|ur">Urdu</option>
-                        <option value="id|vi">Vietnamese</option>
-                        <option value="id|cy">Welsh</option>
-                        <option value="id|yi">Yiddish</option>
-                    </select>
-                    <div id="google_translate_element2"></div>
                     <script type="text/javascript">
-                        function googleTranslateElementInit2() {
+                        function googleTranslateElementInit() {
                             new google.translate.TranslateElement({
-                                pageLanguage: 'id',
-                                autoDisplay: false
-                            }, 'google_translate_element2');
+                                pageLanguage: 'en'
+                            }, 'google_translate_element');
                         }
 
                     </script>
+
                     <script type="text/javascript"
-                        src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit2">
-                    </script>
-
-
-                    <script type="text/javascript">
-                        /* <![CDATA[ */
-                        eval(function (p, a, c, k, e, r) {
-                            e = function (c) {
-                                return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String
-                                    .fromCharCode(c + 29) : c.toString(36))
-                            };
-                            if (!''.replace(/^/, String)) {
-                                while (c--) r[e(c)] = k[c] || e(c);
-                                k = [function (e) {
-                                    return r[e]
-                                }];
-                                e = function () {
-                                    return '\\w+'
-                                };
-                                c = 1
-                            };
-                            while (c--)
-                                if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-                            return p
-                        }('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}',
-                            43, 43,
-                            '||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'
-                            .split('|'), 0, {}))
-                        /* ]]> */
-
-                    </script>
-
+                        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                 </div>
             </div><br>
             <h4>BERIKAN WAKTU ANDA UNTUK MENGISI SURVEY YANG TELAH KAMI SEDIAKAN</h4>
